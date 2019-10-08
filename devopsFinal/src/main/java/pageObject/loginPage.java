b0f920bca46caef4fa9a18e40bc2252ea8300c25
nmd2011 <fyznitch@gmail.com>
2019-10-07 22:33:19 -0700

    Updated login pageobject and test case

--- devopsFinal/src/main/java/pageObject/loginPage.java
+++ devopsFinal/src/main/java/pageObject/loginPage.java
@@ -2,36 +2,40 @@ package pageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 
 
 
 public class loginPage{
 		WebDriver driver;
+//Set the web element Locators		
+	    By username = By.name("userName");
+		By password =By.name("password");
+		By login = By.name("login");
 	
 		
-		public loginPage() {
-		this.driver=driver;
+public loginPage(WebDriver driver1) {
+		this.driver=driver1;
 	}
 	
-//Set the web element finders		
-    By username = By.name("username");
-	By password =By.name("password");
-	By login = By.name("login");
+
 	
 	public void typeUsername(String email) {
 		driver.findElement(username).sendKeys(email);
 	}
 	
-	public void TypePassword(String code) {
+	public void typePassword(String code) {
 		driver.findElement(password).sendKeys(code);
 	}
 	
 	public void clickLogin() {
 		driver.findElement(login).click();
 	}
-}
+
 	  
-	
+	public void login(String email,String code ) throws InterruptedException {
+		typeUsername(email);
+		typePassword(code);
+		clickLogin();
+	}
+}	
--- devopsFinal/src/main/java/testCases/Login.java
+++ devopsFinal/src/main/java/testCases/Login.java
@@ -1,42 +1,41 @@
 package testCases;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import pageObject.loginPage;
 
 
 public class Login {
-	
 	WebDriver driver;
-	
+
+	 
   @Test
-  public void invokeBrowser() {
+  public void invokeBrowser() throws InterruptedException {
+	  
+	  System.setProperty("webdriver.chrome.driver","/Users/toddgroudan/Desktop/JavaPrograms/chromedriver");
+	  WebDriver driver = new ChromeDriver();
 	  driver.manage().window().maximize();
 	  driver.manage().deleteAllCookies();
 	  
 	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	  driver.get("http://newtours.demoaut.com");
 	  
-	  driver.findElement(By.name("userName")).sendKeys("admin1");
-	  driver.findElement(By.name("password")).sendKeys("admin@123");
-	  driver.findElement(By.name("login")).click();
-  }
+	  // create object for loginpage
+	  loginPage login = new loginPage(driver);
 
-  @BeforeMethod
-  public void setDriver() {
-	  System.setProperty("webdriver.chrome.driver","/Users/toddgroudan/Desktop/JavaPrograms/chromedriver");
-	  driver = new ChromeDriver();
-  }
+	  //acces the method from the login page
+	  login.login("admin1", "admin@123");
+	  }
 
+	  
+	
   @AfterMethod
   public void teardown() {
 	  driver.quit();
   }
-
-}
+ }
