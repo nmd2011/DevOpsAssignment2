1eb7721ab281114987c174b96c26d370173011bf
nmd2011 <fyznitch@gmail.com>
2019-10-07 20:13:26 -0700

    Updated the page objects

--- devopsFinal/src/main/java/pageObject/loginPage.java
+++ devopsFinal/src/main/java/pageObject/loginPage.java
@@ -1,5 +1,37 @@
 package pageObject;
 
-public class loginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+
+
+
+public class loginPage{
+		WebDriver driver;
+	
+		
+		public loginPage() {
+		this.driver=driver;
+	}
+	
+//Set the web element finders		
+    By username = By.name("username");
+	By password =By.name("password");
+	By login = By.name("login");
+	
+	public void typeUsername(String email) {
+		driver.findElement(username).sendKeys(email);
+	}
+	
+	public void TypePassword(String code) {
+		driver.findElement(password).sendKeys(code);
+	}
+	
+	public void clickLogin() {
+		driver.findElement(login).click();
+	}
 }
+	  
+	
--- devopsFinal/src/test/java/testCases/Login.java
+++ devopsFinal/src/test/java/testCases/Login.java
@@ -1,9 +1,42 @@
 package testCases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
+
 public class Login {
+	
+	WebDriver driver;
+	
   @Test
-  public void f() {
+  public void invokeBrowser() {
+	  driver.manage().window().maximize();
+	  driver.manage().deleteAllCookies();
+	  
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.get("http://newtours.demoaut.com");
+	  
+	  driver.findElement(By.name("userName")).sendKeys("admin1");
+	  driver.findElement(By.name("password")).sendKeys("admin@123");
+	  driver.findElement(By.name("login")).click();
   }
+
+  @BeforeMethod
+  public void setDriver() {
+	  System.setProperty("webdriver.chrome.driver","/Users/toddgroudan/Desktop/JavaPrograms/chromedriver");
+	  driver = new ChromeDriver();
+  }
+
+  @AfterMethod
+  public void teardown() {
+	  driver.quit();
+  }
+
 }
