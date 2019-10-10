a630dc40532c65a4ecd7ae160e07f2967ed81d3e
nmd2011 <fyznitch@gmail.com>
2019-10-10 07:36:33 -0700

    added the flight finder bones

--- devopsFinal/src/main/java/pageObject/flightFinder.java
+++ devopsFinal/src/main/java/pageObject/flightFinder.java
@@ -1,5 +1,42 @@
 package pageObject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class flightFinder {
+	
+	WebDriver driver;
+	//Set the web element Locators		
+		    By username = By.name("userName");
+			By password =By.name("password");
+			By login = By.name("login");
+			
+ public flightFinder(WebDriver driver1) {
+		
+		}
+		
+
+		
+		public void typeUsername(String email) {
+			Select to = new Select(driver.findElement(.))
+			driver.findElement(username).sendKeys(email);
+		}
+		
+		public void typePassword(String code) {
+			driver.findElement(password).sendKeys(code);
+		}
+		
+		public void clickLogin() {
+			driver.findElement(login).click();
+		}
+
+		  
+		public void login(String email,String code ) throws InterruptedException {
+			typeUsername(email);
+			typePassword(code);
+			clickLogin();
+		
+	}	
 
 }
